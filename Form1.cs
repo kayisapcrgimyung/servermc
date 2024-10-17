2187a2aa6982c43f264722b038be96af4b11789b
kayisapcrgimyung <149445854+kayisapcrgimyung@users.noreply.github.com>
2024-10-17 14:49:48 +0700

    Add files via upload
    Signed-off-by: kayisapcrgimyung <149445854+kayisapcrgimyung@users.noreply.github.com>

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//3
+using System.Data.SqlClient;
+
+namespace tugas1db
+{
+    public partial class Form1 : Form
+    {
+        //4
+        private SqlCommand cmd;
+        private DataSet ds;
+        private SqlDataAdapter da;
+
+        //5
+        koneksi konn = new koneksi();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = konn.GetConn();
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand("Select * from tugas1", conn);
+                ds = new DataSet();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "tugas1");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "tugas1";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+
+            }
+            catch (Exception G)
+            {
+                MessageBox.Show(G.ToString());
+
+            }
+            finally {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ koneksi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//1
+using System.Data.SqlClient;
+
+namespace tugas1db
+{
+    class koneksi
+    {
+        //2
+        public SqlConnection GetConn()
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "Data Source = REVISION-PC\\SQLEXPRESS; initial catalog=DB_KAYIS; integrated security=true";
+            return Conn;
+        }
+    }
+}
